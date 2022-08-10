5088a9e0b96fe71adf09eb685062f111a4a70b84
nguyentdat27 <nguyentdat27@gmail.com>
2022-08-10 15:45:55 +0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,12 +2,12 @@
 export PGPASSWORD='123qwe!!'; 
 set -e
 
-if psql -h hostserver -p 5432 -U postgres -lqt | cut -d \| -f 1 | grep simplcommerce; then
+if psql -h 127.0.0.1 -p 5432 -U postgres -lqt | cut -d \| -f 1 | grep simplcommerce; then
     echo "simplcommerce database existed"
 else
     echo "create new database simplcommerce"
-	psql -h hostserver -p 5432 -U postgres -c "CREATE DATABASE simplcommerce WITH ENCODING 'UTF8'"
-	psql -h hostserver -p 5432 -U postgres -d simplcommerce -a -f /app/dbscript.sql
+	psql -h 127.0.0.1 -p 5432 -U postgres -c "CREATE DATABASE simplcommerce WITH ENCODING 'UTF8'"
+	psql -h 127.0.0.1 -p 5432 -U postgres -d simplcommerce -a -f /app/dbscript.sql
 fi
 
 cd /app && dotnet SimplCommerce.WebHost.dll 
