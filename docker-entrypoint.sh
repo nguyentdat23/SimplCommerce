bb7a820ffb1b212ca8441235575ecd947cc097f3
nguyentdat27 <nguyentdat27@gmail.com>
2022-08-10 13:39:45 +0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-if psql -h simpldb --username postgres -lqt | cut -d \| -f 1 | grep -qw simplcommerce; then
+if sudo -u postgres psql -lqt | cut -d \| -f 1 | grep simplcommerce; then
     echo "simplcommerce database existed"
 else
     echo "create new database simplcommerce"
-	psql -h simpldb --username postgres -c "CREATE DATABASE simplcommerce WITH ENCODING 'UTF8'"
-	psql -h simpldb --username postgres -d simplcommerce -a -f /app/dbscript.sql
+	sudo -u postgres psql -c "CREATE DATABASE simplcommerce WITH ENCODING 'UTF8'"
+	sudo -u postgres psql -d simplcommerce -a -f /app/dbscript.sql
 fi
 
 cd /app && dotnet SimplCommerce.WebHost.dll 
